aad805389281b0c893c7cf877df98fbec97fdae7
pedroski111 <service@fast-jet.com.au>
2018-07-21 13:16:20 +1000

    Add files via upload

---
+++ FinalNode.java
@@ -0,0 +1,12 @@
+package com.fastjet.workflow.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FinalNode {
+	public String value() default "";
+}
---
+++ Parameter.java
@@ -0,0 +1,12 @@
+package com.fastjet.workflow.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Parameter {
+	public int value() default 0;
+}
